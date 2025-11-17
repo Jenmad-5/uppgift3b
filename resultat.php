155daa7e4154c88b3dd1a03031ceaa9b058d1846
”Jennifer <jm224bs@student.lnu.se>
2025-11-17 11:47:29 +0100

    test2

---
+++ resultat.php
@@ -0,0 +1,34 @@
+$namn = $_POST['namn'] ?? '';
+$telefon = $_POST['telefon'] ?? '';
+$prioritet = $_POST['prioritet'] ?? '';
+$ort = $_POST['ort'] ?? '';
+$kategori = $_POST['kategori'] ?? '';
+$rubrik = $_POST['rubrik'] ?? '';
+$beskrivning = $_POST['beskrivning'] ?? '';
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+<meta charset="UTF-8">
+<title>Supportärende</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Supportärende</h1>
+
+<p>Ditt ärende har nu registrerats, vi återkopplar så snart vi kan.</p>
+
+<p><strong>Informationen vi fått är följande:</strong></p>
+
+<p>
+<strong>Namn:</strong> <?= htmlspecialchars($namn) ?><br>
+<strong>Prioritet:</strong> <?= htmlspecialchars($prioritet) ?><br>
+<strong>Telefon:</strong> <?= htmlspecialchars($telefon) ?><br>
+<strong>Ort:</strong> <?= htmlspecialchars($ort) ?><br>
+<strong>Kategori:</strong> <?= htmlspecialchars($kategori) ?><br>
+<strong>Rubrik:</strong> <?= htmlspecialchars($rubrik) ?><br>
+<strong>Beskrivning:</strong> <?= nl2br(htmlspecialchars($beskrivning)) ?>
+</p>
+</body>
+</html>
